4754e989da9462e3e66ac5cda6cf3d105c41f3fa
Vinay Bhat <vinaybhat001@gmail.com>
2015-01-11 16:42:40 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jeeque::Application.config.secret_key_base = 'a2283772ac27971f15efab4981635bbfe36c6e74ed7318bffb1e2992c56af4b97bd5f7a5e1b4d134a4da650d81a0715803340ca69d70231c6b538eeef7ccbddf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jeeque::Application.config.session_store :cookie_store, key: '_jeeque_session'
