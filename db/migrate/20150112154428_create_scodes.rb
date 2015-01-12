96f4677b6cb89735ad4ea3b64e989ba2a9fd2ee9
Vinay Bhat <vinaybhat001@gmail.com>
2015-01-12 21:46:55 +0530

    seed change

--- db/migrate/20150112154428_create_scodes.rb
+++ db/migrate/20150112154428_create_scodes.rb
@@ -1,7 +1,7 @@
 class CreateScodes < ActiveRecord::Migration
   def change
     create_table :scodes do |t|
-      t.integer, :scode
+      t.integer :scode
       t.string :sname
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150111111407) do
+ActiveRecord::Schema.define(version: 20150112154428) do
 
   create_table "questions", force: true do |t|
     t.text     "question"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20150111111407) do
     t.integer  "subjectcode"
     t.integer  "topiccode"
     t.integer  "stopiccode"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "scodes", force: true do |t|
+    t.integer  "scode"
+    t.string   "sname"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
