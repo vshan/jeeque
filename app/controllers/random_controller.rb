7e172a8bbcf225fb878e7999f5bcd2a8fed669af
Vinay Bhat <vinaybhat001@gmail.com>
2015-01-12 20:54:45 +0530

    jugaad

--- app/controllers/random_controller.rb
+++ app/controllers/random_controller.rb
@@ -3,8 +3,6 @@ require 'securerandom'
 class RandomController < ApplicationController
   def enable
     @questions = Question.all
-    @questions2 = @questions[1]
-    @questions
   end
 
   def index
@@ -16,7 +14,40 @@ class RandomController < ApplicationController
   	random
   end
 
+  def generate_question(subject)
+    subq = []
+    enable.each do |question|
+      if question.subjectcode == subject
+        subq << question
+      end 
+    end
+    subq.each {|q| puts q.id }
+    a = []
+    a = [SecureRandom.random_number(5)]
+    while(a.size != 3) do
+      b = SecureRandom.random_number(5)
+      a.each do |e|
+        if e == b
+          a.delete(e)
+        end
+      end
+      a.push(b)
+    end
+    rques = []
+    a.each do |n|
+      subq.each do |question|
+        if n+(5*subject) == question.id 
+          rques << question
+        end
+      end
+    end
+    rques
+  end
+
   def quespaper
+    @phyq = generate_question(1)
+    @chemq = generate_question(2)
+    @matq = generate_question(3)
   end
 
   def random
